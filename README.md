8cf7656abd5de0e95b5b44a0282ff8ad61c1dc58
MPalonek <mikepalonek@gmail.com>
2020-04-30 02:05:44 +0200

    Added more comments and a replacing counter

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 pol-char-replace
+To use this script you need to pass a parameter with the path of subtitles with characters to replace.
+Please make sure that it's encoded in UTF-8, if not use Encoding->Convert to UTF-8 in Notepad++.
+
+Example of use:
+python main.py subtitles.txt
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""Polish character replacer
+
+This script allows user to replace weird special characters into normal polish ones in a text file.
+This will mostly happen when we are dealing with incorrect encoding of a file.
+
+Before running this script, make sure that your file is converted to UTF-8.
+You can do that by editing file in Notepad++ and choosing Encoding->Convert to UTF-8.
+
+If you don't specify path using -o option, script will save it in the same location as
+your source file is in with _converted added to the filename.
+
+Examples of use:
+python main.py subtitles.txt
+python main.py subtitles.txt -o C:\bla\fixed_subtitles.txt
+"""
+
 import sys
 import codecs
 from argparse import ArgumentParser
@@ -12,6 +28,7 @@ CHAR_LIST = [["¹", "ą"],
              ["ñ", "ń"],
              ["Œ", "Ś"],
              ["œ", "ś"],
+             ["", "Ź"],
              ["Ÿ", "ź"],
              ["¯", "Ż"],
              ["¿", "ż"]]
@@ -26,6 +43,7 @@ def parse_arguments():
 
 
 def convert(input, output):
+    #TODO if file is not utf-8 give error and fail the script. Or try to convert it...
     input_f = open(input, "r+", encoding="utf-8", errors='ignore')
     text = input_f.read()
     input_f.close()
@@ -35,13 +53,16 @@ def convert(input, output):
         logging.debug("Replacing {} with {}".format(char[0], char[1]))
         output_text = text.replace(char[0], char[1])
         text = output_text
+        #TODO better counting - if in file we had both characters this will give wrong count
+        logging.debug("Replaced {} times".format(text.count(char[1])))
 
+    #TODO what if there is already file with that name - you shouldn't overwrite it
     output_f = open(output, "w", encoding="utf-8")
     output_f.write(output_text)
     output_f.close()
 
 
-def rename_file(input):
+def get_new_filename(input):
     pos = input.rfind('.')
     output = input[:pos] + "_converted" + input[pos:]
     return output
@@ -57,9 +78,10 @@ def main():
     logging.info('Starting replacing script')
 
     if args.output is None:
-        args.output = rename_file(args.input)
+        args.output = get_new_filename(args.input)
 
     convert(args.input, args.output)
+    logging.info('Finished replacing characters. File is in {}'.format(args.output))
 
 
 if __name__ == "__main__":
